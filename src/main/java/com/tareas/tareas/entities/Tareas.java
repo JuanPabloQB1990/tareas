19297e39405c3d1ed4ee7795f34c7d21822b1bfe
Juan Pablo <juanpabloqb1990@gmail.com>
2024-10-16 19:28:06 -0500

    arreglo constructor

--- src/main/java/com/tareas/tareas/entities/Tareas.java
+++ src/main/java/com/tareas/tareas/entities/Tareas.java
@@ -3,11 +3,13 @@ package com.tareas.tareas.entities;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Data
 @Entity
 @AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "tarea")
 public class Tareas {
 
